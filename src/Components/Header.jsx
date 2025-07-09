a2db72d285fdfcb44f1b119380a6d91e0dd82c86
Andrea Moretti <andrea.moretti1704@gmail.com>
2025-07-09 11:42:09 +0200

    fixes

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,157 +2,194 @@ import { Link, NavLink } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useSearch } from "../Contexts/SearchContext";
 import { useCart } from "../Contexts/CartContext";
-import { faHeart, faCartShopping, faTimes } from "@fortawesome/free-solid-svg-icons";
+import { useState, useEffect } from "react";
+import {
+  faHeart,
+  faCartShopping,
+  faTimes,
+} from "@fortawesome/free-solid-svg-icons";
 import "../assets/css/index.css";
 import SearchBar from "./SearchBar";
 import { useNavigate } from "react-router-dom";
 
 export default function Header() {
-  const navigate= useNavigate()
-    const { cart, removeFromCart} = useCart();
-  const {setSearch} = useSearch();
-  return (<>
-    <nav className="navbar navbar-expand-md navbar-dark custom-navbar">
-      <div className="container-fluid d-flex align-items-center justify-content-between w-100 flex-nowrap">
-        {/* Colonna sinistra: logo */}
-        <div className="navbar-brand d-flex align-items-center">
-          <Link to="/" className="brand-link">
-            <img src="/img/logo.png" alt="logo" className="header-logo" />
-            <h1 className="gradient-text d-none">ZneakDrop</h1>
-          </Link>
-        </div>
+  const navigate = useNavigate();
+  const [canBeOpen, setCanBeOpen] = useState(false);
+  const { cart, removeFromCart } = useCart();
+  const { setSearch } = useSearch();
 
-        {/* Colonna centrale: searchbar */}
-        <div className="searchbar-container">
-          <div className="searchbar-wrapper">
-            <SearchBar />
+  useEffect(() => {
+    if (cart.length) {
+      setCanBeOpen(true);
+    } else {
+      setCanBeOpen(false);
+    }
+  }, [cart]);
+
+  return (
+    <>
+      <nav className="navbar navbar-expand-md navbar-dark custom-navbar">
+        <div className="container-fluid d-flex align-items-center justify-content-between w-100 flex-nowrap">
+          {/* Colonna sinistra: logo */}
+          <div className="navbar-brand d-flex align-items-center">
+            <Link to="/" className="brand-link">
+              <img src="/img/logo.png" alt="logo" className="header-logo" />
+              <h1 className="gradient-text d-none">ZneakDrop</h1>
+            </Link>
           </div>
-        </div>
 
-        {/* Colonna destra: link/menu */}
-        <div className="d-flex align-items-center justify-content-end">
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
+          {/* Colonna centrale: searchbar */}
+          <div className="searchbar-container">
+            <div className="searchbar-wrapper">
+              <SearchBar />
+            </div>
+          </div>
 
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-links d-flex flex-column flex-md-row justify-content-end px-0">
-              <li className="nav-item">
-                <NavLink
-                  to="/"
-                  onClick={() => setSearch('')}
-                  end
-                  className={({ isActive }) => "nav-link" + (isActive ? " active-link" : "")}
-                >
-                  Home
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink
-                  to="/catalog"
-                  onClick={() => setSearch('')}
-                  className={({ isActive }) => "nav-link" + (isActive ? " active-link" : "")}
-                >
-                  Catalogo
-                </NavLink>
-              </li>
-              {/* <li className="nav-item">
-                <NavLink
-                  to="/novelty"
-                  className={({ isActive }) => "nav-link" + (isActive ? " active-link" : "")}
-                >
-                  Novità
-                </NavLink>
-              </li> */}
-              <li className="nav-item">
-                <NavLink
-                  to="/wishlist"
-                  className={({ isActive }) =>
-                    "icon-link wishlist-icon" + (isActive ? " active-link" : "")
-                  }
-                >
-                  <FontAwesomeIcon icon={faHeart} />
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink
-                  data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" aria-controls="offcanvasExample"
-                  className={({ isActive }) =>
-                    "icon-link cart-icon" + (isActive ? " active-link" : "")
-                  }
-                >
-                  <FontAwesomeIcon icon={faCartShopping} />
-                </NavLink>
-              </li>
-            </ul>
-
-            {/* Bottone chiusura mobile */}
+          {/* Colonna destra: link/menu */}
+          <div className="d-flex align-items-center justify-content-end">
             <button
-              className="close-btn d-md-none"
+              className="navbar-toggler"
               type="button"
               data-bs-toggle="collapse"
               data-bs-target="#navbarNav"
-              aria-label="Chiudi menu"
+              aria-controls="navbarNav"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
             >
-              <FontAwesomeIcon icon={faTimes} />
+              <span className="navbar-toggler-icon"></span>
             </button>
+
+            <div className="collapse navbar-collapse" id="navbarNav">
+              <ul className="navbar-links d-flex flex-column flex-md-row justify-content-end px-0">
+                <li className="nav-item">
+                  <NavLink
+                    to="/"
+                    onClick={() => setSearch("")}
+                    end
+                    className={({ isActive }) =>
+                      "nav-link" + (isActive ? " active-link" : "")
+                    }
+                  >
+                    Home
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink
+                    to="/catalog"
+                    onClick={() => setSearch("")}
+                    className={({ isActive }) =>
+                      "nav-link" + (isActive ? " active-link" : "")
+                    }
+                  >
+                    Catalogo
+                  </NavLink>
+                </li>
+
+                <li className="nav-item">
+                  <NavLink
+                    to="/wishlist"
+                    className={({ isActive }) =>
+                      "icon-link wishlist-icon" +
+                      (isActive ? " active-link" : "")
+                    }
+                  >
+                    <FontAwesomeIcon icon={faHeart} />
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink
+                    data-bs-toggle="offcanvas"
+                    data-bs-target={
+                      canBeOpen === true ? "#offcanvasPreviewCart" : " "
+                    }
+                    aria-controls="offcanvasPreviewCart"
+                    className={({ isActive }) =>
+                      "icon-link cart-icon" + (isActive ? " active-link" : "")
+                    }
+                  >
+                    <FontAwesomeIcon icon={faCartShopping} />
+                  </NavLink>
+                </li>
+              </ul>
+
+              {/* Bottone chiusura mobile */}
+              <button
+                className="close-btn d-md-none"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarNav"
+                aria-label="Chiudi menu"
+              >
+                <FontAwesomeIcon icon={faTimes} />
+              </button>
+            </div>
           </div>
         </div>
+      </nav>
+      <div
+        className="offcanvas offcanvas-start"
+        tabIndex="-1"
+        id="offcanvasPreviewCart"
+        aria-labelledby="offcanvasCanvasTitle"
+      >
+        <div className="offcanvas-header">
+          <h5 className="offcanvas-title" id="offcanvasCanvasTitle">
+            Preview carrello
+          </h5>
+          <button
+            type="button"
+            className="btn-close text-reset"
+            data-bs-dismiss="offcanvas"
+            aria-label="Close"
+          ></button>
+        </div>
+        <div className="offcanvas-body">
+          <div className="row g-3">
+            {cart.map((item, index) => (
+              <div
+                className="col-12 d-flex align-items-center border-bottom pb-3"
+                key={index}
+              >
+                <img
+                  src={item.images[0]}
+                  alt={item.model}
+                  width="100"
+                  className="me-3 rounded"
+                />
+                <div className="flex-grow-1">
+                  <Link
+                    to={`/detailpage/${item.slug}`}
+                    className="fw-bold text-dark text-decoration-none"
+                  >
+                    {item.brand} - {item.model}
+                  </Link>
+                  <p className="mt-2">
+                    Taglia: {item.size}
+                    <span className="ms-4">
+                      Prezzo: €{item.price * item.quantity},00
+                    </span>
+                  </p>
+                </div>
+                <button
+                  className="btn btn-outline-danger btn-sm ms-3"
+                  onClick={() => removeFromCart(item.id_sneaker, item.size)}
+                >
+                  Rimuovi
+                </button>
+              </div>
+            ))}
+          </div>
+          <Link
+            data-bs-dismiss="offcanvas"
+            className="btn btn-secondary"
+            onClick={() => {
+              navigate("/cart");
+            }}
+          >
+            Vai alla pagina del carrello
+          </Link>
+        </div>
       </div>
-    </nav>
-    <div className="offcanvas offcanvas-start" tabIndex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
-  <div className="offcanvas-header">
-    <h5 className="offcanvas-title" id="offcanvasExampleLabel">Offcanvas</h5>
-    <button type="button" className="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-  </div>
-  <div className="offcanvas-body">
-   <div className="row g-3">
-                 {cart.map((item, index) => (
-                   <div
-                     className="col-12 d-flex align-items-center border-bottom pb-3"
-                     key={index}
-                   >
-                     <img
-                       src={item.images[0]}
-                       alt={item.model}
-                       width="100"
-                       className="me-3 rounded"
-                     />
-                     <div className="flex-grow-1">
-                       <Link
-                         to={`/detailpage/${item.slug}`}
-                         className="fw-bold text-dark text-decoration-none"
-                       >
-                         {item.brand} - {item.model}
-                       </Link>
-                       <p className="mt-2">
-                         Taglia: {item.size}
-                         <span className="ms-4">
-                           Prezzo: €{item.price * item.quantity},00
-                         </span>
-                       </p>
-                     </div>
-                     <button
-                       className="btn btn-outline-danger btn-sm ms-3"
-                       onClick={() => removeFromCart(item.id_sneaker, item.size)}
-                     >
-                       Rimuovi
-                     </button>
-                  
-                   </div>
-                 ))}
-               </div>  
-                <Link  data-bs-dismiss="offcanvas" className="btn btn-secondary"onClick={()=>{navigate("/cart")}}>Vai alla pagina del carrello</Link>
-  </div>
-</div>
-</>
+    </>
   );
 }
